26cb02a7f5a13f16e519ab0b95514ad1651316a9
Hasibur Rashid <atm.hasibur.rashid20367@gmail.com>
2025-09-30 17:50:24 -0400

    Fix: Final debugging - Add mock data, global types, update tsconfig

---
+++ global.d.ts
@@ -0,0 +1,11 @@
+// Global type declarations for Datinsight
+
+// React Native global
+declare const __DEV__: boolean;
+
+// Expo global
+declare module 'expo-constants' {
+  const Constants: any;
+  export default Constants;
+}
+
